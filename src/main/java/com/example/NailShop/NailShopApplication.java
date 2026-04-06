7aa320f275b6deabb1d09cae4d7aabf769475c42
Luis Miguel <luiskasami@gmail.com>
2026-04-06 17:25:40 -0300

    Parte de Usuario OK

---
+++ src/main/java/com/example/NailShop/NailShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.NailShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NailShop/NailShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NailShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
